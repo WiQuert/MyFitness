6fc7af9db18224a11bc244a6953644d48036fbb6
WiQuert <roshev@slaps.ru>
2023-08-06 20:25:30 +0300

    Add fun setFragment

--- app/src/main/java/com/wiquert/myfitness/MainActivity.kt
+++ app/src/main/java/com/wiquert/myfitness/MainActivity.kt
@@ -2,10 +2,14 @@ package com.wiquert.myfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wiquert.myfitness.fragments.DaysFragment
+import com.wiquert.myfitness.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FragmentManager.setFragment(DaysFragment.newInstance(), this)
     }
 }
+
