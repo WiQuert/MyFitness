8a6df0c710ce62a900aace3ae074f1943d68dadc
WiQuert <roshev@slaps.ru>
2023-08-06 20:22:27 +0300

    Add "Days Fragment" for main screen

---
+++ app/src/main/java/com/wiquert/myfitness/fragments/DaysFragment.kt
@@ -0,0 +1,31 @@
+package com.wiquert.myfitness.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wiquert.myfitness.databinding.FragmentDaysBinding
+
+
+class DaysFragment : Fragment() {
+    private lateinit var binding: FragmentDaysBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDaysBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = DaysFragment()
+
+    }
+
+
+}
