ad2ae0fa3136ae9e8c354362f0b7b6a81cd3a7ec
WiQuert <roshev@slaps.ru>
2023-08-16 19:55:52 +0300

    Initial commit

---
+++ app/src/main/java/com/wiquert/myfitness/adapters/DayModel.kt
@@ -0,0 +1,3 @@
+package com.wiquert.myfitness.adapters
+
+data class DayModel(var exercises: String, var isDone: Boolean)
---
+++ app/src/main/java/com/wiquert/myfitness/adapters/DaysAdapter.kt
@@ -0,0 +1,11 @@
+package com.wiquert.myfitness.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class DaysAdapter : ListAdapter {
+
+    class DayHolder(view: View) : RecyclerView.ViewHolder(view)
+}
