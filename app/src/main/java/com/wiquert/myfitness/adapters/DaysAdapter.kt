5fb170bc49efc72413458ed0db2bba82a9612e98
WiQuert <roshev@slaps.ru>
2023-08-29 19:31:44 +0300

    Add ViewHolder

--- app/src/main/java/com/wiquert/myfitness/adapters/DaysAdapter.kt
+++ app/src/main/java/com/wiquert/myfitness/adapters/DaysAdapter.kt
@@ -4,8 +4,18 @@ import android.view.View
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.wiquert.myfitness.R
+import com.wiquert.myfitness.databinding.DaysListItemBinding
 
-class DaysAdapter : ListAdapter {
+class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder> {
 
-    class DayHolder(view: View) : RecyclerView.ViewHolder(view)
+    class DayHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = DaysListItemBinding.bind(view)
+        fun setData(day: DayModel) = with(binding) {
+            val name = root.context.getString(R.string.day) + " ${adapterPosition} + 1"
+            tvName.text = name
+            val exCounter = day.exercises.split(",").size.toString()
+            tvExCounter.text = exCounter
+        }
+    }
 }
