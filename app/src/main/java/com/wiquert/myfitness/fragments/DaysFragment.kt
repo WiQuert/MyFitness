5aa3ea583bbc31216009f59e138b599264b1bb4e
WiQuert <roshev@slaps.ru>
2023-09-06 20:37:28 +0300

    Add fun fillDaysArray

--- app/src/main/java/com/wiquert/myfitness/fragments/DaysFragment.kt
+++ app/src/main/java/com/wiquert/myfitness/fragments/DaysFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.wiquert.myfitness.R
+import com.wiquert.myfitness.adapters.DayModel
 import com.wiquert.myfitness.databinding.FragmentDaysBinding
 
 
@@ -18,6 +20,16 @@ class DaysFragment : Fragment() {
         binding = FragmentDaysBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+
+    private fun fillDaysArray() : ArrayList<DayModel> {
+
+        val tArray = ArrayList<DayModel>()
+        resources.getStringArray(R.array.day_execrises).forEach {
+            tArray.add(DayModel(it, false))
+        }
+        return tArray
     }
 
     companion object {
