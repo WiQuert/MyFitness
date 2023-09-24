f0b019accde28737bee0d479e6de476239e7f218
WiQuert <roshev@slaps.ru>
2023-09-24 23:27:56 +0300

    Fixes and improvements

--- app/src/main/java/com/wiquert/myfitness/adapters/DaysAdapter.kt
+++ app/src/main/java/com/wiquert/myfitness/adapters/DaysAdapter.kt
@@ -16,7 +16,7 @@ class DaysAdapter : ListAdapter<DayModel, DaysAdapter.DayHolder>(MyComparator())
             fun setData(day: DayModel) = with(binding) {
             val name = root.context.getString(R.string.day) + " ${adapterPosition + 1}"
             tvName.text = name
-            val exCounter = day.exercises.split(",").size.toString()
+            val exCounter = day.exercises.split(",").size.toString() + " " + root.context.getString(R.string.exercise)
             tvExCounter.text = exCounter
         }
     }
