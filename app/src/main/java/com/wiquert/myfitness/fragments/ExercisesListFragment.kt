6a335846c134d29729c03aa02e7f8d2c8fbd55d8
WiQuert <roshev@slaps.ru>
2023-10-25 20:08:29 +0300

    Add Recycler View

--- app/src/main/java/com/wiquert/myfitness/fragments/ExercisesListFragment.kt
+++ app/src/main/java/com/wiquert/myfitness/fragments/ExercisesListFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.wiquert.myfitness.MainActivity
-import com.wiquert.myfitness.R
-import com.wiquert.myfitness.adapters.DayModel
-import com.wiquert.myfitness.adapters.DaysAdapter
+import com.wiquert.myfitness.adapters.ExerciseAdapter
 import com.wiquert.myfitness.databinding.ExercisesListFragmentBinding
-import com.wiquert.myfitness.databinding.FragmentDaysBinding
+import com.wiquert.myfitness.utils.MainViewModel
 
 
 class ExercisesListFragment : Fragment() {
     private lateinit var binding: ExercisesListFragmentBinding
+    private lateinit var adapter: ExerciseAdapter
+    private val model: MainViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +30,19 @@ class ExercisesListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+        model.mutableListExercise.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
 
     }
 
 
+    private fun init() = with(binding) {
+        adapter = ExerciseAdapter()
+        rcView.layoutManager = LinearLayoutManager(activity)
+        rcView.adapter = adapter
+    }
 
     companion object {
 
