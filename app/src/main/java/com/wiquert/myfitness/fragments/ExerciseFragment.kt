1aef907998a3f9bdb27176653711a38141a6899b
WiQuert <roshev@slaps.ru>
2023-11-10 12:17:42 +0300

    Added a part of the screen with the current exercise

--- app/src/main/java/com/wiquert/myfitness/fragments/ExerciseFragment.kt
+++ app/src/main/java/com/wiquert/myfitness/fragments/ExerciseFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wiquert.myfitness.adapters.ExerciseAdapter
 import com.wiquert.myfitness.adapters.ExerciseModel
 import com.wiquert.myfitness.databinding.ExerciseFragmentBinding
+import androidx.fragment.app.activityViewModels
 import com.wiquert.myfitness.databinding.ExercisesListFragmentBinding
 import com.wiquert.myfitness.utils.FragmentManager
 import com.wiquert.myfitness.utils.MainViewModel
+import pl.droidsonroids.gif.GifDrawable
 
 
 class ExerciseFragment : Fragment() {
@@ -40,22 +42,29 @@ class ExerciseFragment : Fragment() {
         model.mutableListExercise.observe(viewLifecycleOwner) {
 
             exList = it
+            nextExercise()
+        }
+        binding.bNext.setOnClickListener {
+            nextExercise()
         }
 
     }
 
     private fun nextExercise() {
 
-        if (exerciseCounter > exList?.size!!) {
-            showExercise()
+        if (exerciseCounter < exList?.size!!) {
+            val ex = exList?.get(exerciseCounter++)
+            showExercise(ex)
         } else {
-
+            Toast.makeText(activity, "Done", Toast.LENGTH_LONG).show()
         }
 
     }
 
-    private fun showExercise(exercise: ExerciseModel) = with(binding) {
-
+    private fun showExercise(exercise: ExerciseModel?) = with(binding) {
+        if (exercise == null) return@with
+        imMain.setImageDrawable(GifDrawable(root.context.assets, exercise.image))
+        tvName.text = exercise.name
     }
 
 
