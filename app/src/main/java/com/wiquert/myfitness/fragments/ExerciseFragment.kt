d09a6c180f4d07451589831acde327559478f469
WiQuert <roshev@slaps.ru>
2023-11-11 16:13:26 +0300

    Added timer for exercises

--- app/src/main/java/com/wiquert/myfitness/fragments/ExerciseFragment.kt
+++ app/src/main/java/com/wiquert/myfitness/fragments/ExerciseFragment.kt
@@ -1,6 +1,7 @@
 package com.wiquert.myfitness.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,7 @@ import androidx.fragment.app.activityViewModels
 import com.wiquert.myfitness.databinding.ExercisesListFragmentBinding
 import com.wiquert.myfitness.utils.FragmentManager
 import com.wiquert.myfitness.utils.MainViewModel
+import com.wiquert.myfitness.utils.TimeUtils
 import pl.droidsonroids.gif.GifDrawable
 
 
@@ -23,6 +25,7 @@ class ExerciseFragment : Fragment() {
     private var exerciseCounter = 0
     private var exList: ArrayList<ExerciseModel>? = null
     private val model: MainViewModel by activityViewModels()
+    private var timer: CountDownTimer? = null
 
 
     override fun onCreateView(
@@ -53,19 +56,56 @@ class ExerciseFragment : Fragment() {
     private fun nextExercise() {
 
         if (exerciseCounter < exList?.size!!) {
-            val ex = exList?.get(exerciseCounter++)
+            val ex = exList?.get(exerciseCounter++) ?: return
             showExercise(ex)
+            setExerciseType(ex)
         } else {
             Toast.makeText(activity, "Done", Toast.LENGTH_LONG).show()
         }
 
     }
 
-    private fun showExercise(exercise: ExerciseModel?) = with(binding) {
-        if (exercise == null) return@with
+    private fun showExercise(exercise: ExerciseModel) = with(binding) {
         imMain.setImageDrawable(GifDrawable(root.context.assets, exercise.image))
         tvName.text = exercise.name
     }
+
+    private fun setExerciseType(exercise: ExerciseModel) {
+        if(exercise.time.startsWith("x")) {
+            binding.tvTime.text = exercise.time
+        } else {
+            startTimer(exercise)
+        }
+    }
+
+        private fun startTimer(exercise: ExerciseModel) = with(binding) {
+            progressBar.max = exercise.time.toInt() * 1000
+            timer?.cancel()
+            timer = object : CountDownTimer(exercise.time.toLong() * 1000,1){
+                override fun onTick(restTime: Long) {
+                    tvTime.text = TimeUtils.getTime(restTime)
+                    progressBar.progress = restTime.toInt()
+
+                }
+
+
+                override fun onFinish() {
+                    nextExercise()
+                }
+
+            }
+                .start()
+
+        }
+
+                override fun onDetach() {
+                    super.onDetach()
+                    timer?.cancel()
+
+        }
+
+
+
 
 
     companion object {
