fc2acab89e41af6866fe4fbf6deb41dcae4866c5
WiQuert <roshev@slaps.ru>
2023-12-04 00:56:14 +0300

    Added saving of completed exercises in each day

--- app/src/main/java/com/wiquert/myfitness/MainActivity.kt
+++ app/src/main/java/com/wiquert/myfitness/MainActivity.kt
@@ -2,13 +2,17 @@ package com.wiquert.myfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.wiquert.myfitness.fragments.DaysFragment
 import com.wiquert.myfitness.utils.FragmentManager
+import com.wiquert.myfitness.utils.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val model: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        model.pref = getSharedPreferences("main", MODE_PRIVATE)
         FragmentManager.setFragment(DaysFragment.newInstance(), this)
     }
 
