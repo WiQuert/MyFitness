776b74b945b81e625acff7a9cf371cc688ac0163
WiQuert <roshev@slaps.ru>
2023-12-15 20:10:23 +0300

    Added display of completed days (numbers and progressbar)

--- app/src/main/java/com/wiquert/myfitness/fragments/DaysFragment.kt
+++ app/src/main/java/com/wiquert/myfitness/fragments/DaysFragment.kt
@@ -50,16 +50,28 @@ class DaysFragment : Fragment(), DaysAdapter.Listener {
     }
 
 
-
-     private fun fillDaysArray() : ArrayList<DayModel> {
-
+    private fun fillDaysArray(): ArrayList<DayModel> {
         val tArray = ArrayList<DayModel>()
+        var daysDoneCounter = 0
         resources.getStringArray(R.array.day_exercises).forEach {
             model.currentDay++
             val exCounter = it.split(",").size
             tArray.add(DayModel(it, 0, model.getExerciseCount() == exCounter))
         }
+        binding.pB.max = tArray.size
+        tArray.forEach {
+            if(it.isDone) daysDoneCounter++
+        }
+        updateRestDaysUI(tArray.size - daysDoneCounter, tArray.size)
         return tArray
+    }
+
+    private fun updateRestDaysUI(restDays: Int, days: Int) = with(binding) {
+        val rDays = getString(R.string.remaining) + " $restDays " + getString(R.string.days)
+        tvRestDays.text = rDays
+        pB.progress = days - restDays
+
+
     }
 
     private fun fillExerciseList(day: DayModel) {
